6cc82ab10c96a06df4ebe606bb1089f56a4dbef0
poppabear8883 <servnx@gmail.com>
2019-05-10 15:19:09 -0400

    ui: Update breadcrumbs to use an icon for the home link

--- src/components/core/Crumbs.js
+++ src/components/core/Crumbs.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Breadcrumb } from 'antd/lib/index';
+import { Breadcrumb, Icon } from 'antd';
 
 class Crumbs extends Component {
   render () {
     return (
       <div className="Crumbs">
-        <Breadcrumb style={{margin: '16px 0'}}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb style={{marginBottom: 25}}>
+          <Breadcrumb.Item href="">
+            <Icon style={{fontSize: 18}} type="home"/>
+          </Breadcrumb.Item>
         </Breadcrumb>
       </div>
     );
